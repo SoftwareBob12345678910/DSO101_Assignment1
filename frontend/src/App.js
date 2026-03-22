c5ec24d580fc009757378a6537b143af35865e6e
SoftwareBob12345678910 <02230284.cst@rub.edu.bt>
2026-03-22 22:52:12 +0600

    chore: change in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+// just for git push
 const API_URL = process.env.REACT_APP_API_URL || 'https://be-todo-02230284.onrender.com';
 
 export default function App() {
